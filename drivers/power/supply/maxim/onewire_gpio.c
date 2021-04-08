2ba858d223390538b696b698965212cef469fc56
Arian <arian.kulmer@web.de>
2021-04-09 00:52:32 +0200

    power: supply: maxim: Properly add support for K6/sweet
    Change-Id: Ib277df4ad71e916bbb134f5c5aa3218ef2322846

--- drivers/power/supply/maxim/onewire_gpio.c
+++ drivers/power/supply/maxim/onewire_gpio.c
@@ -22,9 +22,9 @@
 #include <linux/device.h>
 #include <linux/spinlock.h>
 
-#define ow_info	pr_err
-#define ow_dbg	pr_err
-#define ow_err	pr_err
+#define ow_info	pr_info
+#define ow_dbg	pr_debug
+#define ow_err	pr_debug
 #define ow_log	pr_err
 
 #define DRV_STRENGTH_16MA		(0x7 << 6)
@@ -114,7 +114,11 @@ unsigned char read_bit(void)
 	ONE_WIRE_OUT_LOW;
 	Delay_us(1);////
 	ONE_WIRE_CONFIG_IN;
+#ifdef CONFIG_K6_CHARGE
+	Delay_ns(100);//
+#else
 	Delay_ns(500);//
+#endif
 	vamm = readl_relaxed(g_onewire_data->gpio_in_out_reg); // Read
 	Delay_us(5);
 	ONE_WIRE_OUT_HIGH;
@@ -451,14 +455,14 @@ static int onewire_gpio_probe(struct platform_device *pdev)
 
 	onewire_data->ow_gpio_desc = gpio_to_desc(onewire_data->ow_gpio);
 	onewire_data->ow_gpio_chip = gpiod_to_chip(onewire_data->ow_gpio_desc);
-	
+
 	onewire_data->gpio_in_out_reg = devm_ioremap(&pdev->dev,
 					(uint32_t)onewire_data->onewire_gpio_level_addr, 0x4);
 	onewire_data->gpio_cfg66_reg = devm_ioremap(&pdev->dev,
 					(uint32_t)onewire_data->onewire_gpio_cfg_addr, 0x4);
 	ow_log("onewire_gpio_level_addr is %x; onewire_gpio_cfg_addr is %x", (uint32_t)(onewire_data->onewire_gpio_level_addr), (uint32_t)(onewire_data->onewire_gpio_cfg_addr));
 	ow_log("onewire_data->gpio_cfg66_reg is %x; onewire_data->gpio_in_out_reg is %x", (uint32_t)(onewire_data->gpio_cfg66_reg), (uint32_t)(onewire_data->gpio_in_out_reg));
-	
+
 	// create device node
 	onewire_data->dev = device_create(onewire_class,
 		pdev->dev.parent->parent, onewire_major, onewire_data, "onewirectrl");
