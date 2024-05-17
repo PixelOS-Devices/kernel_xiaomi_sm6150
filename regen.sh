5eae0c5693a645661643562acd6921acd65d76a0
basamaryan <basam.aryan@gmail.com>
2024-05-17 02:24:17 +0200

    VantomKernel: Import scripts
    Change-Id: I0b978eba3451ce20cbeb7e651fc41bec9bff66f2

---
+++ regen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Array of device names
+devices=("sweet" "courbet" "tucana" "toco" "phoenix" "davinci")
+#devices=("sweet" "courbet")
+
+# Loop through the devices
+for device in "${devices[@]}"
+do
+    echo "Generating defconfig for $device"
+    make ARCH=arm64 O=out "${device}"_defconfig
+    make ARCH=arm64 O=out menuconfig
+    cp out/.config "arch/arm64/configs/${device}_defconfig"
+    rm -rf out
+    echo "Defconfig generated for $device"
+    echo ""
+done
+
+echo "Defconfig generation completed for all devices."
